c0fedce229493a04596c806b6ad188cf68cdd57c
Mh-Monzil <monzil246@gmail.com>
2024-07-18 13:22:24 +0600

    text animation

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-gsap.to("#box1", {
-    x: 400,
-    duration: 3,
-    delay: 2,
+gsap.from("h1", {
+    y:20 ,
+    delay:1,
+    opacity: 0,
 })
+
+gsap.to("h1", {
+    color: "crimson"
+})
+
+
+// gsap.to("#box1", {
+//     x: 600,
+//     duration: 2,
+//     delay: 1,
+//     borderRadius: 50,
+// })
+
+// gsap.from("#box2", {
+//     x: 600,
+//     duration: 2,
+//     delay: 1,
+//     borderRadius: 50,
+// })
