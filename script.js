8186f6333c660f85448ebf323950c00d98c51c3e
Mh-Monzil <monzil246@gmail.com>
2024-07-24 21:29:45 +0600

    start and end point of scroll

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-gsap.from("#page3 h1", {
-    opacity:0,
-    duration: 2,
-    x:300,
+// gsap.from("#page3 h1", {
+//     opacity:0,
+//     duration: 2,
+//     x:300,
+//     scrollTrigger: {
+//         trigger: "#page3 h1",
+//         scroller: "body",
+//         markers: true,
+//         start: "top 50%",
+//     }
+// })
+
+
+// gsap.from("#page3 h2", {
+//     opacity:0,
+//     duration: 2,
+//     x:-300,
+//     scrollTrigger: {
+//         trigger: "#page3 h2",
+//         scroller: "body",
+//         markers: true,
+//         start: "top 50%",
+//     }
+// })
+
+gsap.from("#page2 #box", {
+    scale: 0,
+    opacity: 0,
+    duration: 1.5,
+    rotate: 720,
     scrollTrigger: {
-        trigger: "#page3 h1",
+        trigger: "#page2 #box",
         scroller: "body",
         markers: true,
-        start: "top 50%",
+        start: "top 60%",
+        end: "top 30%",
+        scrub: 2,
     }
 })
-
-
-gsap.from("#page3 h2", {
-    opacity:0,
-    duration: 2,
-    x:-300,
-    scrollTrigger: {
-        trigger: "#page3 h2",
-        scroller: "body",
-        markers: true,
-        start: "top 50%",
-    }
-})
-
-
